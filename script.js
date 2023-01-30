6bb0bd8cf87db3f4dbc29e10afb6fe6f025355d9
Gustavo <gusfernandez.g74@gmail.com>
2023-01-29 23:00:01 -0300

    Agregúe los botones y creé las funciones de la calculadora

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function operate(operator, num1, num2) {
+    if (operator === "suma") {
+        
+    } else if (operator === "resta") {
+        
+    } else if (operator === "division") {
+        
+    } else if (operator === "multiplicacion") {
+
+    }
+}
+
+function suma(a, b){
+    let c = a + b
+    return c
+}
+
+function resta(a, b) {
+    let c = a - b
+    return c
+}
+
+function division(a, b) {
+    let c = a/b
+    return c
+}
+
+function multipicacion(a, b) {
+    let c = a * b
+    return c
+}
